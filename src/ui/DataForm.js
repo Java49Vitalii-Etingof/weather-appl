46cab4fec3bd70c699e86663caebea5fefe5de6b
Vitalii <vetingof@gmail.com>
2022-12-20 22:29:06 +0200

    HW#26

--- src/ui/DataForm.js
+++ src/ui/DataForm.js
@@ -1,16 +1,30 @@
+import {showErrorMessage } from "./ErrorMessage.js"
 export class DataForm {
-    //TODO
-    #formElement
-    #dateFromElement
-    #dateToElement
-    #hoursFromElement
-    #hoursToElement
-    #errorMessageElem
+    #formElement;
+    #cityElement;
+    #dateFromElement;
+    #dateToElement;
+    #hourFromElement;
+    #hourToElement;
+    #errorMessageElement;
+    #dateFrom;
+    #dateTo;
+    #hourFrom;
+    #hourTo;
+
     //......
     constructor (params) {
         this.#formElement = document.getElementById(params.idForm);
         this.#dateFromInputElement = document.getElementById(params.idDateFromInput);
-    }   this.#dateToInputElement = document.getElementById(params.idDateToInput);
+        this.#dateToInputElement = document.getElementById(params.idDateToInput);
+        this.#hourFromElement = document.getElementById(params.idHourFrom);
+        this.#hourToElement = document.getElementById(params.idHourTo);
+        this.#errorMessageElement = document.getElementById(params.idErrorMessage);
+        this.onChangeDateFrom();
+        this.onChangeDateTo();
+        this.onChangeHourFrom();
+        this.onChangeHourTo();
+    }   
     addHandler(processFun) {
         //TODO
     }
